63ccf7f770191edac83313007d2c88a944654dd2
Michael Seifert <mseifert@error-reports.org>
2016-02-18 20:26:10 +0100

    Renamed test cases in test_adam.py to better reflect their purpose.

--- tests/test_adam.py
+++ tests/test_adam.py
@@ -1,12 +1,12 @@
 from adam.adam import *
 
-def test_asset_can_be_retrieved():
+def test_contains_asset():
     storage = AssetStorage()
     a = Asset()
     storage['key'] = a
     assert storage['key'] == a
     
-def test_contains_asset():
+def test_contains_key():
     storage = AssetStorage()
     a = Asset()
     assert 'key' not in storage
