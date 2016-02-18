6f08d5bcbd1b8d892d760ce469f79b959989a2a5
Michael Seifert <mseifert@error-reports.org>
2016-02-18 20:36:21 +0100

    AssetStorage stores different versions of an asset, when overwritten.

--- adam/adam.py
+++ adam/adam.py
@@ -1,15 +1,20 @@
+import collections
+
 class AssetStorage:
     def __init__(self):
-        self.assets = {}
+        self.assets = collections.defaultdict(list)
         
     def __setitem__(self, id, asset):
-        self.assets[id] = asset
+        self.assets[id].append(asset)
         
     def __getitem__(self, id):
-        return self.assets[id]
+        return self.assets[id][-1]
     
     def __contains__(self, id):
         return id in self.assets
+    
+    def versions_of(self, id):
+        return self.assets[id]
         
 class Asset:
     def __init__(self):
