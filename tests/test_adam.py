e4940fb183e35cedad979cf64276e75105b72050
Michael Seifert <mseifert@error-reports.org>
2016-02-18 20:22:34 +0100

    Added a test which asserts that an asset under a specified name has is present.

--- tests/test_adam.py
+++ tests/test_adam.py
@@ -5,3 +5,10 @@ def test_asset_can_be_retrieved():
     a = Asset()
     storage['key'] = a
     assert storage['key'] == a
+    
+def test_contains_asset():
+    storage = AssetStorage()
+    a = Asset()
+    assert 'key' not in storage
+    storage['key'] = a
+    assert 'key' in storage
