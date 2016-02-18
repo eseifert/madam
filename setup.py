9c33b550c6443f19f7a110b864d0c95040df5119
Michael Seifert <mseifert@error-reports.org>
2016-02-18 20:08:43 +0100

    Added basic setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='ADAM',
+    version='0.1',
+    author='Michael Seifert',
+    author_email='mseifert@error-reports.org',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
