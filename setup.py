e1e10b5778a857ccd7786518a56f33ba28401365
Michael Seifert <mseifert@error-reports.org>
2016-02-20 14:14:24 +0100

    Added pytaglib to the install dependencies.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     version='0.1',
     author='Michael Seifert',
     author_email='mseifert@error-reports.org',
+    install_requires=['pytaglib'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
