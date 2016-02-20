d606c5929e659496da243ef87ad1cecb94aab7cc
Michael Seifert <mseifert@error-reports.org>
2016-02-20 15:30:59 +0100

    Factored out AssetStorage as a test fixture.

--- tests/test_adam.py
+++ tests/test_adam.py
@@ -2,21 +2,22 @@ import pytest
 
 from adam.adam import *
 
-def test_contains_asset():
-    storage = AssetStorage()
+@pytest.fixture
+def storage():
+    return AssetStorage()
+
+def test_contains_asset(storage):
     a = Asset()
     storage['key'] = a
     assert storage['key'] == a
     
-def test_contains_key():
-    storage = AssetStorage()
+def test_contains_key(storage):
     a = Asset()
     assert 'key' not in storage
     storage['key'] = a
     assert 'key' in storage
     
-def test_asset_is_versioned():
-    storage = AssetStorage()
+def test_asset_is_versioned(storage):
     a = Asset()
     updated_a = Asset()
     storage['key'] = a
@@ -26,15 +27,13 @@ def test_asset_is_versioned():
     assert versions[0] == a
     assert versions[1] == updated_a
 
-def test_asset_is_deleted():
-    storage = AssetStorage()
+def test_asset_is_deleted(storage):
     a = Asset()
     storage['key'] = a
     del storage['key']
     assert 'key' not in storage
 
-def test_deleting_unkown_key_raises_exception():
-    storage = AssetStorage()
+def test_deleting_unkown_key_raises_exception(storage):
     with pytest.raises(KeyError):
         del storage['key']
 
