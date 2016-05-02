c5a463749280379bf155efc4c56690fca03cd5fa
Michael Seifert <mseifert@error-reports.org>
2016-05-02 17:29:19 +0200

    Improved readbility of image tests by expanding the test fixture.

--- tests/test_image.py
+++ tests/test_image.py
@@ -11,7 +11,7 @@ def test_supports_jfif():
 
 
 @pytest.fixture
-def jpeg_rgb_image():
+def jpeg_asset():
     empty_image = PIL.Image.new('RGB', (1, 1))
     image_data = io.BytesIO()
     empty_image.save(image_data, 'JPEG')
@@ -22,48 +22,37 @@ def jpeg_rgb_image():
     image_with_exif_metadata = io.BytesIO()
     piexif.insert(exif_bytes, image_data.read(), image_with_exif_metadata)
 
-    return image_with_exif_metadata
+    jpeg_asset = adam.image.read_jpeg(image_with_exif_metadata)
+    return jpeg_asset
 
 
-def test_read_jpeg_returns_asset_with_jpeg_mime_type(jpeg_rgb_image):
-    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
-
+def test_read_jpeg_returns_asset_with_jpeg_mime_type(jpeg_asset):
     assert jpeg_asset.mime_type == 'image/jpeg'
 
 
-def test_jpeg_asset_essence_is_filled(jpeg_rgb_image):
-    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
-
+def test_jpeg_asset_essence_is_filled(jpeg_asset):
     assert jpeg_asset.essence is not None
 
 
-def test_jpeg_asset_contains_size_information(jpeg_rgb_image):
-    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
-
+def test_jpeg_asset_contains_size_information(jpeg_asset):
     assert jpeg_asset.width == 1
     assert jpeg_asset.height == 1
 
 
-def test_jpeg_asset_essence_is_a_jpeg(jpeg_rgb_image):
-    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
-
+def test_jpeg_asset_essence_is_a_jpeg(jpeg_asset):
     jpeg_image = PIL.Image.open(jpeg_asset.essence)
 
     assert jpeg_image.format == 'JPEG'
 
 
-def test_jpeg_asset_essence_can_be_read_multiple_times(jpeg_rgb_image):
-    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
-
+def test_jpeg_asset_essence_can_be_read_multiple_times(jpeg_asset):
     essence_contents = jpeg_asset.essence.read()
     same_essence_contents = jpeg_asset.essence.read()
 
     assert essence_contents == same_essence_contents
 
 
-def test_jpeg_asset_essence_does_not_contain_exif_metadata(jpeg_rgb_image):
-    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
-    jpeg_asset.essence.seek(0)
+def test_jpeg_asset_essence_does_not_contain_exif_metadata(jpeg_asset):
     essence_bytes = jpeg_asset.essence.read()
 
     essence_exif = piexif.load(essence_bytes)
