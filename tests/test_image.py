4e27015f0ca038234e3460decab203e0ba0f024e
Michael Seifert <mseifert@error-reports.org>
2016-05-02 14:46:13 +0200

    Image tests use BytesIO instead of tempfiles.

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,5 +1,5 @@
 import adam.image
-import tempfile
+import io
 import PIL.Image
 
 def test_supports_jfif():
@@ -8,20 +8,20 @@ def test_supports_jfif():
 def test_read_jpeg_returns_asset_with_jpeg_mime_type():
     # Given
     empty_image = PIL.Image.new('RGB', (1, 1))
-    with tempfile.NamedTemporaryFile(suffix='.jpg') as tmp:
-        jpeg_file = empty_image.save(tmp.name, 'JPEG')
-        # When
-        jpeg_asset = adam.image.readJpeg(tmp.name)
+    image_data = io.BytesIO()
+    jpeg_file = empty_image.save(image_data, 'JPEG')
+    # When
+    jpeg_asset = adam.image.readJpeg(image_data)
     # Then
     assert jpeg_asset.mime_type =='image/jpeg'
 
 def test_jpeg_asset_essence_is_filled():
     # Given
     empty_image = PIL.Image.new('RGB', (1, 1))
-    with tempfile.NamedTemporaryFile(suffix='.jpg') as tmp:
-        jpeg_file = empty_image.save(tmp.name, 'JPEG')
-        # When
-        jpeg_asset = adam.image.readJpeg(tmp.name)
+    image_data = io.BytesIO()
+    jpeg_file = empty_image.save(image_data, 'JPEG')
+    # When
+    jpeg_asset = adam.image.readJpeg(image_data)
     # Then
     assert jpeg_asset.essence != None
 
