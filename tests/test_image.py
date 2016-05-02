8b18a8306f852ad7f2affb8930be329bb2b4f59a
Michael Seifert <mseifert@error-reports.org>
2016-05-02 15:16:24 +0200

    Simplified code in image tests by using a test fixture.

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,27 +1,31 @@
+import pytest
+
 import adam.image
 import io
 import PIL.Image
 
+
 def test_supports_jfif():
     assert 'image/jpeg' in adam.supported_mime_types
-    
-def test_read_jpeg_returns_asset_with_jpeg_mime_type():
-    # Given
-    empty_image = PIL.Image.new('RGB', (1, 1))
-    image_data = io.BytesIO()
-    jpeg_file = empty_image.save(image_data, 'JPEG')
-    # When
-    jpeg_asset = adam.image.read_jpeg(image_data)
-    # Then
-    assert jpeg_asset.mime_type =='image/jpeg'
 
-def test_jpeg_asset_essence_is_filled():
-    # Given
+
+@pytest.fixture
+def jpeg_rgb_image():
     empty_image = PIL.Image.new('RGB', (1, 1))
     image_data = io.BytesIO()
-    jpeg_file = empty_image.save(image_data, 'JPEG')
-    # When
-    jpeg_asset = adam.image.read_jpeg(image_data)
-    # Then
-    assert jpeg_asset.essence != None
+    empty_image.save(image_data, 'JPEG')
+    image_data.seek(0)
+    return image_data
+
+
+def test_read_jpeg_returns_asset_with_jpeg_mime_type(jpeg_rgb_image):
+    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
+
+    assert jpeg_asset.mime_type == 'image/jpeg'
+
+
+def test_jpeg_asset_essence_is_filled(jpeg_rgb_image):
+    jpeg_asset = adam.image.read_jpeg(jpeg_rgb_image)
+
+    assert jpeg_asset.essence is not None
 
