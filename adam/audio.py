a20e8cf19b511ebbeeed5fcf796066b172bc811c
Michael Seifert <mseifert@error-reports.org>
2016-05-13 19:57:58 +0200

    read_mp3 and read_wav have been wrapped in WaveProcessor and MutagenProcessor, respectively.

--- adam/audio.py
+++ adam/audio.py
@@ -7,38 +7,49 @@ import tempfile
 import wave
 
 
-@supports_mime_types('audio/vnd.wave', 'audio/wav', 'audio/wave', 'audio/x-wav')
-def read_wav(wave_file):
-    asset = Asset()
-    asset['mime_type'] = 'audio/wav'
-    with wave.open(wave_file) as wave_data:
-        asset['channels'] = wave_data.getnchannels()
-        asset['framerate'] = wave_data.getframerate()
-        essence_bytes = wave_data.readframes(wave_data.getnframes())
-        essence_stream = io.BytesIO()
-        essence_stream.write(essence_bytes)
-        essence_stream.seek(0)
-        asset.essence = essence_stream
-    return asset
+@supports_mime_types()
+class WaveProcessor:
+    @staticmethod
+    def read(wave_file):
+        asset = Asset()
+        asset['mime_type'] = 'audio/wav'
+        with wave.open(wave_file) as wave_data:
+            asset['channels'] = wave_data.getnchannels()
+            asset['framerate'] = wave_data.getframerate()
+            essence_bytes = wave_data.readframes(wave_data.getnframes())
+            essence_stream = io.BytesIO()
+            essence_stream.write(essence_bytes)
+            essence_stream.seek(0)
+            asset.essence = essence_stream
+        return asset
+
+    @staticmethod
+    def can_read():
+        return ['audio/vnd.wave', 'audio/wav', 'audio/wave', 'audio/x-wav']
 
 
-@supports_mime_types('audio/mpeg')
-def read_mp3(mp3_file):
-    asset = Asset()
-    asset['mime_type'] = 'audio/mpeg'
+@supports_mime_types()
+class MutagenProcessor:
+    @staticmethod
+    def read(mp3_file):
+        asset = Asset()
+        asset['mime_type'] = 'audio/mpeg'
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        file_path = mp3_file.name
-        filename = os.path.basename(file_path)
-        copy_path = os.path.join(temp_dir, filename)
-        shutil.copyfile(file_path, copy_path)
-    
-        mp3 = mutagen.mp3.MP3(copy_path)
-        asset['duration'] = mp3.info.length
-        mp3.tags.delete()
-        
-        with open(copy_path, 'rb') as mp3_file_copy:
-            asset.essence = mp3_file_copy
-    return asset
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = mp3_file.name
+            filename = os.path.basename(file_path)
+            copy_path = os.path.join(temp_dir, filename)
+            shutil.copyfile(file_path, copy_path)
 
+            mp3 = mutagen.mp3.MP3(copy_path)
+            asset['duration'] = mp3.info.length
+            mp3.tags.delete()
+
+            with open(copy_path, 'rb') as mp3_file_copy:
+                asset.essence = mp3_file_copy
+        return asset
+
+    @staticmethod
+    def can_read():
+        return ['audio/mpeg']
 
