184b4657e100e17767f2175a2c77f8dc34abe537
Michael Seifert <mseifert@error-reports.org>
2016-05-13 15:56:16 +0200

    Removed read_jpeg as a public module function of adam.

--- adam/__init__.py
+++ adam/__init__.py
@@ -1,6 +1,5 @@
 from .core import read
 from .core import read_method_by_mime_type
 from .audio import read_mp3, read_wav
-from .image import read_jpeg
 
 supported_mime_types = read_method_by_mime_type.keys()
