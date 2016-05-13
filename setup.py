81d28c708783af6d7358f950a0eb62ab619adc73
Michael Seifert <mseifert@error-reports.org>
2016-05-13 18:01:44 +0200

    Removed obsolete dependency on pytaglib in setup.py.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1',
     author='Michael Seifert',
     author_email='mseifert@error-reports.org',
-    install_requires=['piexif', 'pillow', 'pytaglib'],
+    install_requires=['piexif', 'pillow'],
     setup_requires=['pytest-runner'],
     tests_require=['piexif', 'pillow', 'pytest'],
 )
