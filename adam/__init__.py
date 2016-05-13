2191db9c138ac3ea0826c27117b37f9e20121f85
Michael Seifert <mseifert@error-reports.org>
2016-05-13 19:52:38 +0200

    read_mp3 and read_wav are no longer functions in module scope.

--- adam/__init__.py
+++ adam/__init__.py
@@ -1,5 +1,4 @@
 from .core import read
 from .core import read_method_by_mime_type
-from .audio import read_mp3, read_wav
 
 supported_mime_types = read_method_by_mime_type.keys()
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,8 +1,8 @@
-import adam
+import adam.audio
 
 
 def test_create_asset_from_wav():
-    asset = adam.read_wav('tests/16-bit-mono.wav')
+    asset = adam.audio.read_wav('tests/16-bit-mono.wav')
     assert asset['mime_type'] == 'audio/wav'
     assert asset['framerate'] == 48000
     assert asset['channels'] == 1
@@ -12,7 +12,7 @@ def test_create_asset_from_wav():
 def test_create_asset_from_mp3():
     mp3_file_path = 'tests/64kbits.mp3'
     with open(mp3_file_path, 'rb') as mp3_file:
-        asset = adam.read_mp3(mp3_file)
+        asset = adam.audio.read_mp3(mp3_file)
     assert asset['mime_type'] == 'audio/mpeg'
     assert asset['duration'] == 0.1
     assert asset.essence is not None
@@ -27,7 +27,7 @@ def test_mp3_reader_does_not_change_file():
     with open(mp3_file_path, 'rb') as mp3_file:
         expected_data = mp3_file.read()
     with open(mp3_file_path, 'rb') as mp3_file:
-        adam.read_mp3(mp3_file)
+        adam.audio.read_mp3(mp3_file)
     with open(mp3_file_path, 'rb') as mp3_file:
         actual_data = mp3_file.read()
     assert expected_data == actual_data
