4123831720c449c58445d73e7debd3de360c4899
Erich Seifert <dev@erichseifert.de>
2016-08-06 16:34:40 +0200

    Added test for unkown video file format.

--- tests/test_video.py
+++ tests/test_video.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import pytest
 
 import madam.video
@@ -15,6 +16,15 @@ def test_supports_y4m_file(ffmpeg_processor):
         supported = ffmpeg_processor.can_read(video_file)
 
     assert supported
+
+
+def test_cannot_read_unknown_file(ffmpeg_processor):
+    random_data = b'\x07]>e\x10\n+Y\x07\xd8\xf4\x90%\r\xbbK\xb8+\xf3v%\x0f\x11'
+    unknown_file = BytesIO(random_data)
+
+    supported = ffmpeg_processor.can_read(unknown_file)
+
+    assert not supported
 
 
 def test_read_returns_asset_when_called_with_video_file(ffmpeg_processor):
