c63ab402584067ecef727049e758027ffb95029f
Michael Seifert <mseifert@error-reports.org>
2016-08-11 15:38:11 +0200

    Module-level tests no longer depend on the exif fixture of the assets module.

--- tests/test_madam.py
+++ tests/test_madam.py
@@ -6,13 +6,14 @@ import pytest
 
 import madam
 from madam.core import UnsupportedFormatError
-from assets import jpeg_asset, exif
+from assets import jpeg_asset
 
 
-def test_jpeg_asset_essence_does_not_contain_exif_metadata(exif):
-    jpeg_data = io.BytesIO()
-    piexif.insert(piexif.dump(exif), jpeg_asset().essence.read(), new_file=jpeg_data)
-    asset = madam.read(jpeg_data)
+def test_jpeg_asset_essence_does_not_contain_exif_metadata():
+    exif = jpeg_asset().metadata['exif']
+    data_with_exif = io.BytesIO()
+    piexif.insert(piexif.dump(exif), jpeg_asset().essence.read(), new_file=data_with_exif)
+    asset = madam.read(data_with_exif)
     essence_bytes = asset.essence.read()
 
     essence_exif = piexif.load(essence_bytes)
