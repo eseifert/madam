4b012242f539462decb31b7b80ff6a18e6b60546
Erich Seifert <dev@erichseifert.de>
2016-08-26 16:02:29 +0200

    Added project URL, descriptions, plattforms, and license information to setup.py.

--- setup.py
+++ setup.py
@@ -4,8 +4,14 @@ from setuptools import setup
 
 import versioneer
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name='MADAM',
+    description='Digital asset management library',
+    long_description=long_description,
+    url='https://github.com/eseifert/madam',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     author='Michael Seifert, Erich Seifert',
@@ -17,6 +23,8 @@ setup(
         'doc': ['sphinx >=1.3', 'sphinx_rtd_theme']
     },
     packages=['madam'],
+    platforms=['POSIX'],
+    license='AGPLv3',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -25,7 +33,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Multimedia :: Graphics :: Graphics Conversion',
         'Topic :: Multimedia :: Sound/Audio :: Conversion',
-        'Topic :: Multimedia :: Video :: Graphics Conversion',
+        'Topic :: Multimedia :: Video :: Conversion',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.4',
