0ce31f951b11a5437a77dd58c1b67ccfb4cba843
Michael Seifert <mseifert@error-reports.org>
2016-08-27 11:27:19 +0200

    Grouped video tests for FFmpegProcessor.

--- tests/test_video.py
+++ tests/test_video.py
@@ -4,40 +4,37 @@ import pytest
 import madam.video
 
 
-@pytest.fixture
-def ffmpeg_processor():
-    return madam.video.FFmpegProcessor()
+class TestFFmpegProcessor:
+    @pytest.fixture
+    def ffmpeg_processor(self):
+        return madam.video.FFmpegProcessor()
 
+    def test_supports_y4m_file(self, ffmpeg_processor):
+        video_path = 'tests/resources/bus_qcif_15fps.y4m'
 
-def test_supports_y4m_file(ffmpeg_processor):
-    video_path = 'tests/resources/bus_qcif_15fps.y4m'
+        with open(video_path, 'rb') as video_file:
+            supported = ffmpeg_processor.can_read(video_file)
 
-    with open(video_path, 'rb') as video_file:
-        supported = ffmpeg_processor.can_read(video_file)
+        assert supported
 
-    assert supported
+    def test_cannot_read_unknown_file(self, ffmpeg_processor):
+        random_data = b'\x07]>e\x10\n+Y\x07\xd8\xf4\x90%\r\xbbK\xb8+\xf3v%\x0f\x11'
+        unknown_file = BytesIO(random_data)
 
+        supported = ffmpeg_processor.can_read(unknown_file)
 
-def test_cannot_read_unknown_file(ffmpeg_processor):
-    random_data = b'\x07]>e\x10\n+Y\x07\xd8\xf4\x90%\r\xbbK\xb8+\xf3v%\x0f\x11'
-    unknown_file = BytesIO(random_data)
+        assert not supported
 
-    supported = ffmpeg_processor.can_read(unknown_file)
+    def test_fails_with_invalid_file_object(self, ffmpeg_processor):
+        invalid_file = None
 
-    assert not supported
+        with pytest.raises(ValueError):
+            ffmpeg_processor.can_read(invalid_file)
 
+    def test_read_returns_asset_when_called_with_video_file(self, ffmpeg_processor):
+        video_path = 'tests/resources/bus_qcif_15fps.y4m'
 
-def test_fails_with_invalid_file_object(ffmpeg_processor):
-    invalid_file = None
+        with open(video_path, 'rb') as video_file:
+            asset = ffmpeg_processor.read(video_file)
 
-    with pytest.raises(ValueError):
-        ffmpeg_processor.can_read(invalid_file)
-
-
-def test_read_returns_asset_when_called_with_video_file(ffmpeg_processor):
-    video_path = 'tests/resources/bus_qcif_15fps.y4m'
-
-    with open(video_path, 'rb') as video_file:
-        asset = ffmpeg_processor.read(video_file)
-
-    assert asset is not None
+        assert asset is not None
