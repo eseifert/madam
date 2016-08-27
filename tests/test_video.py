50e51441b8a25ed8dc543b7375c3437fd6781b3d
Michael Seifert <mseifert@error-reports.org>
2016-08-27 11:29:14 +0200

    Video tests make use of fixture renaming to prevent shadowing of fixture functions.

--- tests/test_video.py
+++ tests/test_video.py
@@ -5,36 +5,36 @@ import madam.video
 
 
 class TestFFmpegProcessor:
-    @pytest.fixture
+    @pytest.fixture(name='processor')
     def ffmpeg_processor(self):
         return madam.video.FFmpegProcessor()
 
-    def test_supports_y4m_file(self, ffmpeg_processor):
+    def test_supports_y4m_file(self, processor):
         video_path = 'tests/resources/bus_qcif_15fps.y4m'
 
         with open(video_path, 'rb') as video_file:
-            supported = ffmpeg_processor.can_read(video_file)
+            supported = processor.can_read(video_file)
 
         assert supported
 
-    def test_cannot_read_unknown_file(self, ffmpeg_processor):
+    def test_cannot_read_unknown_file(self, processor):
         random_data = b'\x07]>e\x10\n+Y\x07\xd8\xf4\x90%\r\xbbK\xb8+\xf3v%\x0f\x11'
         unknown_file = BytesIO(random_data)
 
-        supported = ffmpeg_processor.can_read(unknown_file)
+        supported = processor.can_read(unknown_file)
 
         assert not supported
 
-    def test_fails_with_invalid_file_object(self, ffmpeg_processor):
+    def test_fails_with_invalid_file_object(self, processor):
         invalid_file = None
 
         with pytest.raises(ValueError):
-            ffmpeg_processor.can_read(invalid_file)
+            processor.can_read(invalid_file)
 
-    def test_read_returns_asset_when_called_with_video_file(self, ffmpeg_processor):
+    def test_read_returns_asset_when_called_with_video_file(self, processor):
         video_path = 'tests/resources/bus_qcif_15fps.y4m'
 
         with open(video_path, 'rb') as video_file:
-            asset = ffmpeg_processor.read(video_file)
+            asset = processor.read(video_file)
 
         assert asset is not None
