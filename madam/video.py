d08de06b22b628284da9a17956d18794738bea69
Michael Seifert <mseifert@error-reports.org>
2016-08-27 13:30:50 +0200

    Deduplicated code in FFmpegProcessor by creating an abstraction for the ffprobe command. Resolves #27.

--- madam/video.py
+++ madam/video.py
@@ -26,23 +26,28 @@ class FFmpegProcessor(Processor):
     """
     Represents a processor that uses FFmpeg to read audio and video data.
     """
+
+    class _FFprobe:
+        def show_format(self, file):
+            with tempfile.NamedTemporaryFile() as tmp:
+                tmp.write(file.read())
+                command = 'ffprobe -print_format json -loglevel quiet -show_format'.split()
+                command.append(tmp.name)
+                result = _run(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            string_result = result.stdout.decode('utf-8')
+            json_obj = json.loads(string_result)
+            return json_obj.get('format', None)
+
+    def __init__(self):
+        self._ffprobe = self._FFprobe()
+
     def can_read(self, file):
         if not file:
             raise ValueError('Error when reading file-like object: %r' % file)
-        with tempfile.NamedTemporaryFile() as tmp:
-            tmp.write(file.read())
-            ffprobe = _run(('ffprobe -print_format json -show_format -loglevel quiet %s' % tmp.name).split(),
-                           stdin=subprocess.PIPE, input=file.read(), stdout=subprocess.PIPE)
-        string_result = ffprobe.stdout.decode('utf-8')
-        json_obj = json.loads(string_result)
-        return bool(json_obj)
+        json_result = self._ffprobe.show_format(file)
+        return bool(json_result)
 
     def read(self, file):
-        with tempfile.NamedTemporaryFile() as tmp:
-            tmp.write(file.read())
-            ffprobe = _run(('ffprobe -print_format json -show_format -loglevel quiet %s' % tmp.name).split(),
-                           stdout=subprocess.PIPE, stdin=subprocess.PIPE, input=file.read())
-        string_result = ffprobe.stdout.decode('utf-8')
-        json_obj = json.loads(string_result)
+        json_result = self._ffprobe.show_format(file)
         file.seek(0)
-        return Asset(essence=file, duration=float(json_obj['format']['duration']))
+        return Asset(essence=file, duration=float(json_result['duration']))
