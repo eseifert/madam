b7056e13e2d9ecbac3927065a003033455e8a08e
Michael Seifert <mseifert@error-reports.org>
2016-08-27 12:51:56 +0200

    Deduplicated code in FFmpegProcessor.

--- madam/video.py
+++ madam/video.py
@@ -1,3 +1,4 @@
+import collections
 import json
 import subprocess
 import tempfile
@@ -5,42 +6,41 @@ import tempfile
 from madam.core import Asset, Processor
 
 
+_CompletedProcess = collections.namedtuple('_CompletedProcess', ['args', 'retcode', 'stdout', 'stderr'])
+
+
+def _run(command, stdin=None, input=None, stdout=None, stderr=None):
+    with subprocess.Popen(command, stdin=stdin,
+                          stdout=stdout, stderr=stderr) as process:
+        try:
+            stdout, stderr = process.communicate(input=input)
+        except:
+            process.kill()
+            process.wait()
+            raise
+        retcode = process.poll()
+    return _CompletedProcess(args=process.args, retcode=retcode, stdout=stdout, stderr=stderr)
+
+
 class FFmpegProcessor(Processor):
     """
     Represents a processor that uses FFmpeg to read audio and video data.
     """
     def can_read(self, file):
-        ffprobe_cmd = 'ffprobe -print_format json -show_format -loglevel quiet -'
-        with subprocess.Popen(ffprobe_cmd.split(), stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE) as process:
-            try:
-                stdout, stderr = process.communicate(input=file.read())
-            except:
-                process.kill()
-                process.wait()
-                raise ValueError('Error when reading file-like object: %r' % file)
-            retcode = process.wait()
-            if retcode:
-                return False
-        string_result = stdout.decode('utf-8')
+        if not file:
+            raise ValueError('Error when reading file-like object: %r' % file)
+        ffprobe = _run('ffprobe -print_format json -show_format -loglevel quiet -'.split(),
+                       stdin=subprocess.PIPE, input=file.read(), stdout=subprocess.PIPE)
+        string_result = ffprobe.stdout.decode('utf-8')
         json_obj = json.loads(string_result)
         return bool(json_obj)
 
     def read(self, file):
         with tempfile.NamedTemporaryFile() as tmp:
             tmp.write(file.read())
-            ffprobe_cmd = 'ffprobe -print_format json -show_entries format=duration -loglevel quiet %s' % tmp.name
-            with subprocess.Popen(ffprobe_cmd.split(), stdout=subprocess.PIPE) as process:
-                try:
-                    stdout, stderr = process.communicate(input=file.read())
-                except:
-                    process.kill()
-                    process.wait()
-                    raise ValueError('Error when reading file-like object: %r' % file)
-                retcode = process.wait()
-                if retcode:
-                    return False
-        string_result = stdout.decode('utf-8')
+            ffprobe = _run(('ffprobe -print_format json -show_entries format=duration -loglevel quiet %s' % tmp.name).split(),
+                           stdout=subprocess.PIPE, stdin=subprocess.PIPE, input=file.read())
+        string_result = ffprobe.stdout.decode('utf-8')
         json_obj = json.loads(string_result)
         file.seek(0)
         return Asset(essence=file, duration=float(json_obj['format']['duration']))
