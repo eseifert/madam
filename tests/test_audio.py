8f32e70191b9a8ae89fd0dbc6693c0f62af16205
Erich Seifert <dev@erichseifert.de>
2016-08-29 00:30:28 +0200

    Grouped test cases for WaveProcessor and MutagenProcessor in test classes.

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,33 +1,46 @@
+import pytest
+
 import madam.audio
 
 
-def test_create_asset_from_wav():
-    asset = madam.audio.WaveProcessor()._read('tests/resources/16-bit-mono.wav')
-    assert asset.mime_type == 'audio/wav'
-    assert asset.framerate == 48000
-    assert asset.channels == 1
-    assert asset.essence is not None
+class TestWaveProcessor:
+    @pytest.fixture(name='processor')
+    def wave_processor(self):
+        return madam.audio.WaveProcessor()
+
+    def test_create_asset_from_wav(self, processor):
+        asset = processor._read('tests/resources/16-bit-mono.wav')
+        assert asset.mime_type == 'audio/wav'
+        assert asset.framerate == 48000
+        assert asset.channels == 1
+        assert asset.essence is not None
+        assert asset.essence.read()
 
 
-def test_create_asset_from_mp3():
-    mp3_file_path = 'tests/resources/64kbits.mp3'
-    with open(mp3_file_path, 'rb') as mp3_file:
-        asset = madam.audio.MutagenProcessor()._read(mp3_file)
-    assert asset.mime_type == 'audio/mpeg'
-    assert asset.duration == 0.1
-    assert asset.essence is not None
+class TestMutagenProcessor:
+    @pytest.fixture(name='processor')
+    def mutagen_processor(self):
+        return madam.audio.MutagenProcessor()
 
-    # Assert that essence was stripped from metadata
-    with open(mp3_file_path, 'rb') as mp3_file:
-        assert asset.essence != mp3_file.read()
+    def test_create_asset_from_mp3(self, processor):
+        mp3_file_path = 'tests/resources/64kbits.mp3'
+        with open(mp3_file_path, 'rb') as mp3_file:
+            asset = processor._read(mp3_file)
+        assert asset.mime_type == 'audio/mpeg'
+        assert asset.duration == 0.1
+        assert asset.essence is not None
+        assert asset.essence.read()
 
+        # Assert that essence was stripped from metadata
+        with open(mp3_file_path, 'rb') as mp3_file:
+            assert asset.essence.read() != mp3_file.read()
 
-def test_mp3_reader_does_not_change_file():
-    mp3_file_path = 'tests/resources/64kbits.mp3'
-    with open(mp3_file_path, 'rb') as mp3_file:
-        expected_data = mp3_file.read()
-    with open(mp3_file_path, 'rb') as mp3_file:
-        madam.audio.MutagenProcessor()._read(mp3_file)
-    with open(mp3_file_path, 'rb') as mp3_file:
-        actual_data = mp3_file.read()
-    assert expected_data == actual_data
+    def test_mp3_reader_does_not_change_file(self, processor):
+        mp3_file_path = 'tests/resources/64kbits.mp3'
+        with open(mp3_file_path, 'rb') as mp3_file:
+            expected_data = mp3_file.read()
+        with open(mp3_file_path, 'rb') as mp3_file:
+            processor._read(mp3_file)
+        with open(mp3_file_path, 'rb') as mp3_file:
+            actual_data = mp3_file.read()
+        assert expected_data == actual_data
