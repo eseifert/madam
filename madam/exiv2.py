3e75d9121abcd053dc5c8fb989d6448cedb9f12c
Michael Seifert <mseifert@error-reports.org>
2016-08-30 09:46:14 +0200

    Exiv2Processor.read returns exif information.

--- madam/exiv2.py
+++ madam/exiv2.py
@@ -1,3 +1,8 @@
+import tempfile
+
+import pyexiv2
+
+
 class Exiv2Processor:
     """
     Represents a metadata processor using the exiv2 library.
@@ -5,3 +10,14 @@ class Exiv2Processor:
     @property
     def format(self):
         return 'exif'
+
+    def read(self, file):
+        with tempfile.NamedTemporaryFile() as tmp:
+            tmp.write(file.read())
+            tmp.flush()
+            metadata = pyexiv2.ImageMetadata(tmp.name)
+            metadata.read()
+        exif = {}
+        for key in metadata.exif_keys:
+            exif[key] = metadata[key]
+        return metadata
--- tests/test_exiv2.py
+++ tests/test_exiv2.py
@@ -1,5 +1,8 @@
+import io
+import pyexiv2
 import pytest
 
+from assets import jpeg_asset
 from exiv2 import Exiv2Processor
 
 
@@ -10,3 +13,15 @@ class TestExiv2Processor:
 
     def test_format_is_exif(self, processor):
         assert processor.format == 'exif'
+
+    def test_read_returns_exif_dict_when_jpeg_contains_exif(self, processor, jpeg_asset, tmpdir):
+        file = tmpdir.join('asset_with_exif.jpg')
+        file.write(jpeg_asset.essence.read(), 'wb')
+        metadata = pyexiv2.metadata.ImageMetadata(str(file))
+        metadata.read()
+        metadata['Exif.Image.Artist'] = b'Test artist'
+        metadata.write()
+
+        exif = processor.read(io.BytesIO(file.read('rb')))
+
+        assert exif
