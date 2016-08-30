d7e060a021811948f10521c6c0fed1fe52b752d4
Michael Seifert <mseifert@error-reports.org>
2016-08-30 09:24:28 +0200

    Grouped exiv2 tests into a class.

--- tests/test_exiv2.py
+++ tests/test_exiv2.py
@@ -3,10 +3,10 @@ import pytest
 from exiv2 import Exiv2Processor
 
 
-@pytest.fixture(name='processor')
-def exiv2_processor():
-    return Exiv2Processor()
+class TestExiv2Processor:
+    @pytest.fixture(name='processor')
+    def exiv2_processor(self):
+        return Exiv2Processor()
 
-
-def test_format_is_exif(processor):
-    assert processor.format == 'exif'
+    def test_format_is_exif(self, processor):
+        assert processor.format == 'exif'
