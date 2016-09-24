87bd40880c9fbb4006eecde7b8c900addf5cd32d
Erich Seifert <dev@erichseifert.de>
2016-09-25 01:34:51 +0200

    test assets: y4m_asset is created using the yuv444p pixel format.

--- tests/assets.py
+++ tests/assets.py
@@ -189,7 +189,7 @@ def mp4_asset(tmpdir_factory):
 def y4m_asset():
     duration = 0.2
     command = ('ffmpeg -loglevel error -f lavfi -i color=color=red:duration=%.1f:rate=15 '
-               '-f yuv4mpegpipe pipe:' % duration).split()
+               '-pix_fmt yuv444p -f yuv4mpegpipe pipe:' % duration).split()
     ffmpeg = subprocess_run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return madam.core.Asset(essence=io.BytesIO(ffmpeg.stdout),
                             mime_type='video/x-yuv4mpegpipe',
