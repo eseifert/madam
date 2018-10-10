f2989e53308789908c626194f3817e6b414f61ed
Erich Seifert <dev@erichseifert.de>
2018-10-10 19:50:01 +0200

    Tests: Renamed madam fixture to manager to avoid shadowing of "madam" module.

--- tests/test_madam.py
+++ tests/test_madam.py
@@ -46,35 +46,35 @@ class TestAsset:
 
 
 class TestMadam:
-    @pytest.fixture(name='madam', scope='class')
+    @pytest.fixture(name='manager', scope='class')
     def madam_instance(self):
         return Madam()
 
     @pytest.fixture(scope='class')
-    def read_asset(self, madam, asset):
-        return madam.read(asset.essence)
+    def read_asset(self, manager, asset):
+        return manager.read(asset.essence)
 
-    def test_get_processor_returns_processor_for_readable_asset(self, madam, asset):
-        processor = madam.get_processor(asset.essence)
+    def test_get_processor_returns_processor_for_readable_asset(self, manager, asset):
+        processor = manager.get_processor(asset.essence)
 
         assert processor is not None
 
-    def test_get_processor_returns_none_for_unreadable_asset(self, madam, unknown_asset):
-        processor = madam.get_processor(unknown_asset.essence)
+    def test_get_processor_returns_none_for_unreadable_asset(self, manager, unknown_asset):
+        processor = manager.get_processor(unknown_asset.essence)
 
         assert processor is None
 
-    def test_read_returns_jpeg_asset_with_correct_metadata(self, madam, jpeg_data_with_exif):
+    def test_read_returns_jpeg_asset_with_correct_metadata(self, manager, jpeg_data_with_exif):
         jpeg_with_metadata = jpeg_data_with_exif
 
-        asset = madam.read(jpeg_with_metadata)
+        asset = manager.read(jpeg_with_metadata)
 
         assert 'exif' in asset.metadata
 
-    def test_read_returns_jpeg_asset_whose_essence_does_not_contain_metadata(self, madam, jpeg_image_asset, tmpdir):
+    def test_read_returns_jpeg_asset_whose_essence_does_not_contain_metadata(self, manager, jpeg_image_asset, tmpdir):
         jpeg_with_metadata = jpeg_image_asset
 
-        asset = madam.read(jpeg_with_metadata.essence)
+        asset = manager.read(jpeg_with_metadata.essence)
 
         essence_file = tmpdir.join('essence_without_metadata.jpg')
         essence_file.write(asset.essence.read(), 'wb')
@@ -82,34 +82,34 @@ class TestMadam:
         metadata.read()
         assert not metadata
 
-    def test_read_empty_file_raises_error(self, madam):
+    def test_read_empty_file_raises_error(self, manager):
         file_data = io.BytesIO()
 
         with pytest.raises(UnsupportedFormatError):
-            madam.read(file_data)
+            manager.read(file_data)
 
-    def test_read_raises_when_file_is_none(self, madam):
+    def test_read_raises_when_file_is_none(self, manager):
         invalid_file = None
 
         with pytest.raises(TypeError):
-            madam.read(invalid_file)
+            manager.read(invalid_file)
 
-    def test_read_raises_error_when_format_is_unknown(self, madam, unknown_asset):
+    def test_read_raises_error_when_format_is_unknown(self, manager, unknown_asset):
         with pytest.raises(UnsupportedFormatError):
-            madam.read(unknown_asset.essence)
+            manager.read(unknown_asset.essence)
 
     def test_read_returns_asset_when_reading_valid_data(self, read_asset):
         assert read_asset is not None
 
-    def test_read_returns_asset_with_correct_mime_type(self, madam, asset):
-        read_asset = madam.read(asset.essence)
+    def test_read_returns_asset_with_correct_mime_type(self, manager, asset):
+        read_asset = manager.read(asset.essence)
 
         assert read_asset.mime_type == asset.mime_type
 
-    def test_read_stores_additional_metadata(self, madam, asset):
+    def test_read_stores_additional_metadata(self, manager, asset):
         filename = 'foobar'
 
-        read_asset = madam.read(asset.essence, additional_metadata=dict(filename=filename))
+        read_asset = manager.read(asset.essence, additional_metadata=dict(filename=filename))
 
         assert 'filename' in read_asset.metadata
         assert read_asset.filename == filename
@@ -117,71 +117,71 @@ class TestMadam:
     def test_read_returns_asset_whose_essence_is_filled(self, read_asset):
         assert read_asset.essence.read()
 
-    def test_read_returns_image_asset_with_correct_color_mode(self, madam, image_asset):
+    def test_read_returns_image_asset_with_correct_color_mode(self, manager, image_asset):
         asset = image_asset
 
-        read_asset = madam.read(asset.essence)
+        read_asset = manager.read(asset.essence)
 
         assert read_asset.color_space == asset.color_space
         assert read_asset.depth == asset.depth
         assert read_asset.data_type == asset.data_type
 
-    def test_read_jpeg_does_not_alter_the_original_file(self, madam):
+    def test_read_jpeg_does_not_alter_the_original_file(self, manager):
         jpeg_data = jpeg_image_asset().essence
         original_image_data = jpeg_data.read()
         jpeg_data.seek(0)
 
-        madam.read(jpeg_data)
+        manager.read(jpeg_data)
 
         jpeg_data.seek(0)
         image_data_after_reading = jpeg_data.read()
         assert original_image_data == image_data_after_reading
 
-    def test_read_video_returns_asset_with_duration_metadata(self, madam, video_asset):
-        asset = madam.read(video_asset.essence)
+    def test_read_video_returns_asset_with_duration_metadata(self, manager, video_asset):
+        asset = manager.read(video_asset.essence)
 
         assert asset.duration == pytest.approx(video_asset.duration, rel=0.4)
 
-    def test_read_video_returns_asset_containing_video_size_metadata(self, madam, video_asset):
-        asset = madam.read(video_asset.essence)
+    def test_read_video_returns_asset_containing_video_size_metadata(self, manager, video_asset):
+        asset = manager.read(video_asset.essence)
 
         assert asset.width == DEFAULT_WIDTH
         assert asset.height == DEFAULT_HEIGHT
 
-    def test_read_returns_video_asset_containing_video_stream_metadata(self, madam, video_asset):
+    def test_read_returns_video_asset_containing_video_stream_metadata(self, manager, video_asset):
         asset = video_asset
 
-        read_asset = madam.read(asset.essence)
+        read_asset = manager.read(asset.essence)
 
         assert 'video' in read_asset.metadata
 
-    def test_read_returns_video_asset_containing_color_mode_metadata(self, madam, video_asset):
+    def test_read_returns_video_asset_containing_color_mode_metadata(self, manager, video_asset):
         asset = video_asset
 
-        read_asset = madam.read(asset.essence)
+        read_asset = manager.read(asset.essence)
 
         assert read_asset.video['color_space'] == asset.video['color_space']
         assert read_asset.video['depth'] == asset.video['depth']
         assert read_asset.video['data_type'] == asset.video['data_type']
 
-    def test_read_returns_asset_containing_image_size_metadata(self, madam, image_asset):
+    def test_read_returns_asset_containing_image_size_metadata(self, manager, image_asset):
         image_data = image_asset.essence
 
-        asset = madam.read(image_data)
+        asset = manager.read(image_data)
 
         assert asset.width == DEFAULT_WIDTH
         assert asset.height == DEFAULT_HEIGHT
 
-    def test_read_returns_image_asset_containing_color_depth_metadata(self, madam, image_asset):
+    def test_read_returns_image_asset_containing_color_depth_metadata(self, manager, image_asset):
         image_data = image_asset.essence
 
-        asset = madam.read(image_data)
+        asset = manager.read(image_data)
 
         assert isinstance(asset.metadata['depth'], int)
         assert asset.metadata['depth'] > 0
         assert asset.metadata['depth'] == image_asset.metadata['depth']
 
-    def test_read_return_correct_hierarchy_of_metadata(self, madam, jpeg_image_asset, tmpdir):
+    def test_read_return_correct_hierarchy_of_metadata(self, manager, jpeg_image_asset, tmpdir):
         file = tmpdir.join('asset_with_metadata.jpg')
         file.write(jpeg_image_asset.essence.read(), 'wb')
         metadata = pyexiv2.metadata.ImageMetadata(str(file))
@@ -190,13 +190,13 @@ class TestMadam:
         metadata['Iptc.Application2.Caption'] = ['Bar']
         metadata.write()
 
-        asset = madam.read(file.open('rb'))
+        asset = manager.read(file.open('rb'))
 
         assert 'exif' not in asset.metadata
         assert 'iptc' in asset.metadata
         assert len(asset.iptc) == 2
 
-    def test_read_only_returns_python_types_in_metadata(self, madam, jpeg_image_asset, tmpdir):
+    def test_read_only_returns_python_types_in_metadata(self, manager, jpeg_image_asset, tmpdir):
         import datetime, fractions, frozendict
         allowed_types = {str, float, int, tuple, frozendict.frozendict,
                          datetime.datetime, fractions.Fraction}
@@ -208,7 +208,7 @@ class TestMadam:
         metadata['Iptc.Application2.Caption'] = ['Foo bar']
         metadata.write()
 
-        asset = madam.read(file.open('rb'))
+        asset = manager.read(file.open('rb'))
 
         for metadata_type in {'exif', 'iptc'}:
             values = asset.metadata[metadata_type].values()
@@ -216,32 +216,32 @@ class TestMadam:
             for value in values:
                 assert type(value) in allowed_types
 
-    def test_writes_correct_essence_without_metadata(self, madam, asset):
+    def test_writes_correct_essence_without_metadata(self, manager, asset):
         asset = Asset(essence=asset.essence)
         file = io.BytesIO()
 
-        madam.write(asset, file)
+        manager.write(asset, file)
 
         file.seek(0)
         assert file.read() == asset.essence.read()
 
-    def test_writes_correct_essence_with_metadata(self, madam, jpeg_image_asset):
+    def test_writes_correct_essence_with_metadata(self, manager, jpeg_image_asset):
         file = io.BytesIO()
 
-        madam.write(jpeg_image_asset, file)
+        manager.write(jpeg_image_asset, file)
 
         file.seek(0)
         assert file.read() != jpeg_image_asset.essence.read()
 
-    def test_config_contains_list_of_all_processors_by_default(self, madam):
-        assert madam.config['processors'] == [
+    def test_config_contains_list_of_all_processors_by_default(self, manager):
+        assert manager.config['processors'] == [
             'madam.image.PillowProcessor',
             'madam.vector.SVGProcessor',
             'madam.ffmpeg.FFmpegProcessor',
         ]
 
-    def test_config_contains_list_of_all_metadata_processors_by_default(self, madam):
-        assert madam.config['metadata_processors'] == [
+    def test_config_contains_list_of_all_metadata_processors_by_default(self, manager):
+        assert manager.config['metadata_processors'] == [
             'madam.exiv2.Exiv2MetadataProcessor',
             'madam.vector.SVGMetadataProcessor',
             'madam.ffmpeg.FFmpegMetadataProcessor',
